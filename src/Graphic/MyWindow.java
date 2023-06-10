7f5af2b08181cdee00e55df2ba4ded1bc512503e
evilraptor <nikulin5.80mail@mail.ru>
2023-06-10 14:09:43 +0700

    RedPotato7

--- src/Graphic/MyWindow.java
+++ src/Graphic/MyWindow.java
@@ -1,123 +1,81 @@
 package Graphic;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.*;
 
-public class MyWindow extends JFrame implements ActionListener {
-    private JLabel portLabel, ipLabel;
-    private JTextField portTextField, ipTextField;
-    private JButton serverButton, clientButton;
-
-    private JPanel panel1,panel2,panel3;
+public class MyWindow extends JFrame {
+    private JButton serverButton,clientButton;
+    private JTextField textField1, textField2;
 
     public MyWindow() {
-        super("chat application");
+        super("the chat server/client window");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // установка размеров окна
-        setSize(400, 200);
+        // Создание кнопки
+        serverButton = new JButton("start the server!");
+        clientButton = new JButton("join a server!");
+        serverButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //clientButton.setVisible(false);//FIXME здесь кнопка вторая про другой режим удаляется слишком рано
 
-        // создание панелей для размещения компонентов
-        panel1 = new JPanel(new FlowLayout());
-        panel2 = new JPanel(new FlowLayout());
-        panel3 = new JPanel(new BorderLayout());
+                // Создание панели для ввода текста
+                JPanel panel = new JPanel();
+                panel.add(new JLabel("write the server's port"));
+                textField1 = new JTextField(10);
+                panel.add(textField1);
 
-        // создание кнопок и добавление обработчика событий
-        serverButton = new JButton("Server");
-        serverButton.addActionListener(this);
-        clientButton = new JButton("Client");
-        clientButton.addActionListener(this);
+                // Создание диалогового окна для ввода текста
+                int result = JOptionPane.showConfirmDialog(MyWindow.this, panel, "creating the server", JOptionPane.OK_CANCEL_OPTION);
 
+                if (result == JOptionPane.OK_OPTION) {
+                    // Создание нового экземпляра класса А и вызов его метода
+                    A a = new A();
+                    a.printText(textField1.getText(), textField2.getText());
+                }
+            }
+        });
+        clientButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Создание панели для ввода текста
+                JPanel panel = new JPanel();
+                panel.add(new JLabel("write the server's ip"));
+                textField1 = new JTextField(10);
+                panel.add(textField1);
+                panel.add(new JLabel("write the server's port"));
+                textField2 = new JTextField(10);
+                panel.add(textField2);
 
-        /*portLabel = new JLabel("port:");
-        ipLabel = new JLabel("ip:");
+                // Создание диалогового окна для ввода текста
+                int result = JOptionPane.showConfirmDialog(MyWindow.this, panel, "joining the server", JOptionPane.OK_CANCEL_OPTION);
 
-        portTextField = new JTextField(15);
-        ipTextField = new JTextField(15);*/
+                if (result == JOptionPane.OK_OPTION) {
+                    // Создание нового экземпляра класса А и вызов его метода
+                    A a = new A();
+                    a.printText(textField1.getText(), textField2.getText());
+                }
+            }
+        });
 
-        /*// создание кнопок и добавление обработчика событий
-        buttonA = new JButton("A");
-        buttonA.addActionListener(this);
-        buttonB = new JButton("B");
-        buttonB.addActionListener(this);
+        // Добавление компонентов на панель
+        JPanel panel = new JPanel();
+        panel.add(serverButton);
+        panel.add(clientButton);
 
-        // создание панелей для размещения компонентов
-        JPanel panel1 = new JPanel(new FlowLayout());
-        JPanel panel2 = new JPanel(new FlowLayout());
-        JPanel panel3 = new JPanel(new BorderLayout());*/
+        // Добавление панели на окно
+        getContentPane().add(panel);
 
-        /*// добавление компонентов на первую панель
-        panel1.add(portLabel);
-        panel1.add(portTextField);
-
-        // добавление компонентов на вторую панель
-        panel2.add(ipLabel);
-        panel2.add(ipTextField);*/
-
-        // добавление кнопок на третью панель
-        JPanel panelButtons = new JPanel(new FlowLayout());
-        panelButtons.add(serverButton);
-        panelButtons.add(clientButton);
-
-        // добавление панелей на основную панель
-        panel3.add(panel1, BorderLayout.NORTH);
-        panel3.add(panel2, BorderLayout.CENTER);
-        panel3.add(panelButtons, BorderLayout.SOUTH);
-
-        // добавление основной панели на окно
-        setContentPane(panel3);
-
-        // отображение окна
+        // Настройка размеров окна и его видимости
+        setSize(300, 400);
         setVisible(true);
     }
 
-    // обработчик событий для кнопок A и B
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == serverButton) {
-            // создание компонентов
-            portLabel = new JLabel("port:");
-            portTextField = new JTextField(15);
-
-            // добавление компонентов на первую панель
-            panel1.add(portLabel);
-            panel1.add(portTextField);
-
-            // запуск сервера
-            new A(portTextField.getText());
-        } else if (e.getSource() == clientButton) {
-            // создание компонентов
-            portLabel = new JLabel("port:");
-            ipLabel = new JLabel("ip:");
-
-            portTextField = new JTextField(15);
-            ipTextField = new JTextField(15);
-
-            // добавление компонентов на первую панель
-            panel1.add(portLabel);
-            panel1.add(portTextField);
-
-            // добавление компонентов на вторую панель
-            panel2.add(ipLabel);
-            panel2.add(ipTextField);
-
-            // запуск клиента
-            new B(portTextField.getText(), ipTextField.getText());
-        }
+    public static void main(String[] args) {
+        new MyWindow();
     }
 }
 
 class A {
-    public A(String text1) {
-        System.out.println("Класс A запущен.");
-        System.out.println("Текст 1: " + text1);
-    }
-}
-
-class B {
-    public B(String text1, String text2) {
-        System.out.println("Класс B запущен.");
-        System.out.println("Текст 1: " + text1);
-        System.out.println("Текст 2: " + text2);
+    public void printText(String text1, String text2) {
+        System.out.println(text1 + " " + text2);
     }
 }
