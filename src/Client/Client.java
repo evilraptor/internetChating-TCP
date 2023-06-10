e45200b6bf01f3bf2f4fff63d7933f5aa07002de
evilraptor <nikulin5.80mail@mail.ru>
2023-06-10 14:48:56 +0700

    RedPotato9

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,10 +1,7 @@
 package Client;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
 public class Client {
     public static void main(String[] args) throws IOException {
